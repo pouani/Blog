feab997d8fe718a16ba1e985dd7ac4135d876483
Ulrich POUANI <pouani@medicasure.com>
2023-02-15 13:13:03 +0100

    Rendering List

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,15 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import React, { useContext } from 'react'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
 
-  const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
 
   return (
     <View>
       <Text>IndexScreen</Text>
-      <Text>{value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
     </View>
   )
 }
