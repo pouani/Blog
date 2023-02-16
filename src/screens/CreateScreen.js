7d08301e322fb680893c88e285b8471678a8fcc6
Ulrich POUANI <pouani@medicasure.com>
2023-02-16 16:08:05 +0100

    Displaying a Form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,16 +1,45 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
 
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = ({ navigation }) => {
+  const [ title, setTitle ] = useState('');
+  const [ content, setContent ] = useState('');
+
   return (
     <View>
-      <Text>CreateScreen</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput 
+        value={title} 
+        onChangeText={(text) => setTitle(text)}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput 
+        value={content} 
+        onChangeText={(text) => setTitle(text)}
+        style={styles.input}
+      />
+      <Button title="Add Blog Post" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  }
+});
 
 export default CreateScreen;
