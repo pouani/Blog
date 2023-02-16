67dafbbcdc1475eec9e0b9f5bdf175633217efec
Ulrich POUANI <pouani@medicasure.com>
2023-02-16 12:52:27 +0100

    Adding a Creation Screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+import { Context } from '../context/BlogContext';
+
+const CreateScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>CreateScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
