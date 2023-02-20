3656aa78ff650d24d3a78ce02be7682d56c12ae6
Ulrich POUANI <pouani@medicasure.com>
2023-02-20 16:50:53 +0100

    Initializing State from Context

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useContext, useState } from 'react'
+
+import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ navigation }) => {
+
+  const { state } = useContext(Context)
+
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam('id')
+  );
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>EditScreen - {navigation.getParam('id')}</Text>
+      <Text>Edit Title:</Text>
+      <TextInput value={title} onChange={newTitle => setTitle(newTitle)} />
+      <Text>Edit Content:</Text>
+      <TextInput value={content} onChange={newContent => setContent(newContent)} />
     </View>
   )
 }
